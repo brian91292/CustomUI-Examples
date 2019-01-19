72f137a1601ddb546d03cb4b64015ffe4dcbfa72
brian91292 <8050523+brian91292@users.noreply.github.com>
2019-01-19 13:51:08 -0600

    fix MenuCreation example

--- CustomUI Examples/MenuCreation.cs
+++ CustomUI Examples/MenuCreation.cs
@@ -12,26 +12,30 @@ namespace CustomUI_Examples
 {
     class MenuCreation : MonoBehaviour
     {
+        public static CustomMenu TestMenu = null;
         public static void CreateNewMenu()
         {
-            //Creating the view controller with a BACK BUTTON!!!
-            var menu = BeatSaberUI.CreateCustomMenu<CustomMenu>("Testing New Menu"); //Required, the string here is the name that will appear at the top of the view controller
-            var viewController = BeatSaberUI.CreateViewController<CustomViewController>(); //Required
-            menu.SetMainViewController(viewController, true, (firstActivation, type) => //Required
+            if (TestMenu == null)
             {
-                //Content you want on the view controller
+                //Creating the view controller with a BACK BUTTON!!!
+                TestMenu = BeatSaberUI.CreateCustomMenu<CustomMenu>("Testing New Menu"); //Required, the string here is the name that will appear at the top of the view controller
+                var viewController = BeatSaberUI.CreateViewController<CustomViewController>(); //Required
+                TestMenu.SetMainViewController(viewController, true, (firstActivation, type) => //Required
+                {
+                    //Content you want on the view controller
 
-                Button bigboibutton = viewController.CreateUIButton("SoloFreePlayButton", new Vector2(60f, 20f));
-                bigboibutton.SetButtonText("Big Boi (with icon!)");
-                //bigboibutton.SetButtonIcon(UIUtilities.LoadSpriteFromResources("CustomUI_Examples.Resources.IMAGE.png")); this button is for setting a new icon. 
-                //You'll want to add the image as a resoures and set it to embed on build.
+                    Button bigboibutton = viewController.CreateUIButton("SoloFreePlayButton", new Vector2(60f, 20f));
+                        bigboibutton.SetButtonText("Big Boi (with icon!)");
+                    //bigboibutton.SetButtonIcon(UIUtilities.LoadSpriteFromResources("CustomUI_Examples.Resources.IMAGE.png")); this button is for setting a new icon. 
+                    //You'll want to add the image as a resoures and set it to embed on build.
 
-                //Text Element
-                TextMeshProUGUI randomtext = viewController.CreateText("<b>Bold Button</b>", new Vector2(0f, 34f)); //Text Mesh Pro supports rich text!!!
-                randomtext.fontSize = 10f; //Change font, font is different from the font size of buttons
-                randomtext.alignment = TextAlignmentOptions.Center; //Changing Alignment
-            });
-            menu.Present(); //Required
+                    //Text Element
+                    TextMeshProUGUI randomtext = viewController.CreateText("<b>Bold Button</b>", new Vector2(0f, 34f)); //Text Mesh Pro supports rich text!!!
+                    randomtext.fontSize = 10f; //Change font, font is different from the font size of buttons
+                    randomtext.alignment = TextAlignmentOptions.Center; //Changing Alignment
+                });
+            }
+            TestMenu.Present(); //Required
         }
     }
 }
